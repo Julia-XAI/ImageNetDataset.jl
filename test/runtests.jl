1b038867192386237ddf7aeca7813b1d16f67817
adrhill <adrian.hill@mailbox.org>
2026-01-07 15:42:59 +0100

    Prepare formatting with Runic

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,14 +2,9 @@ using ImageNetDataset
 using Test
 using Aqua
 using JET
-using JuliaFormatter
 using ReferenceTests
 
 @testset "ImageNetDataset.jl" begin
-    @testset "Code formatting (JuliaFormatter.jl)" begin
-        @info "Testing code formatting (JuliaFormatter.jl)..."
-        @test format(ImageNetDataset; verbose=false, overwrite=false)
-    end
     @testset "Code quality (Aqua.jl)" begin
         @info "Testing code quality (Aqua.jl)..."
         Aqua.test_all(ImageNetDataset; ambiguities=false)
