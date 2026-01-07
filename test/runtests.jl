4e2ce1af2f202b315f4491d26677af5374778da3
Adrian Hill <adrian.hill@mailbox.org>
2026-01-07 16:09:39 +0100

    Chore: format with Runic, update CI (#22)

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,22 +2,17 @@ using ImageNetDataset
 using Test
 using Aqua
 using JET
-using JuliaFormatter
 using ReferenceTests
 
 @testset "ImageNetDataset.jl" begin
-    @testset "Code formatting (JuliaFormatter.jl)" begin
-        @info "Testing code formatting (JuliaFormatter.jl)..."
-        @test format(ImageNetDataset; verbose=false, overwrite=false)
-    end
     @testset "Code quality (Aqua.jl)" begin
         @info "Testing code quality (Aqua.jl)..."
-        Aqua.test_all(ImageNetDataset; ambiguities=false)
+        Aqua.test_all(ImageNetDataset; ambiguities = false)
     end
     @testset "Code linting (JET.jl)" begin
         @info "Testing code linting (JET.jl)..."
         if VERSION >= v"1.10"
-            JET.test_package(ImageNetDataset; target_defined_modules=true)
+            JET.test_package(ImageNetDataset; target_defined_modules = true)
         end
     end
     @testset "Preprocessing" begin
--- test/test_preprocessing.jl
+++ test/test_preprocessing.jl
@@ -10,7 +10,7 @@ A = transform(tfm, path)
 img = inverse_transform(tfm, A)
 @test_reference "references/CenterCropNormalize_default_cat.txt" img
 
-tfm = CenterCropNormalize(; output_size=(60, 40))
+tfm = CenterCropNormalize(; output_size = (60, 40))
 A = transform(tfm, path)
 @test size(A) == (60, 40, 3)
 @test_reference "references/CenterCropNormalize_100_50.txt" A
@@ -19,10 +19,10 @@ img = inverse_transform(tfm, A)
 
 ## Normalization
 default_mean = (0.485f0, 0.456f0, 0.406f0)
-default_std  = (0.229f0, 0.224f0, 0.225f0)
+default_std = (0.229f0, 0.224f0, 0.225f0)
 
 std = (1.0f0, 2.0f0, 3.0f0)
-tfm = CenterCropNormalize(; output_size=(60, 40), std=std)
+tfm = CenterCropNormalize(; output_size = (60, 40), std = std)
 A = transform(tfm, path)
 @test size(A) == (60, 40, 3)
 @test_reference "references/CenterCropNormalize_100_50_std.txt" A
@@ -36,7 +36,7 @@ A = transform(tfm, path)
 img = inverse_transform(tfm, A)
 @test size(img) == (224, 224)
 
-tfm = RandomCropNormalize(; output_size=(60, 40))
+tfm = RandomCropNormalize(; output_size = (60, 40))
 A = transform(tfm, path)
 @test size(A) == (60, 40, 3)
 img = inverse_transform(tfm, A)
